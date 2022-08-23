24f9389d756692ea5d34df05a9ce96177f8f50b1
justguang <2320366520@qq.com>
2022-08-23 18:21:28 +0800

    1.暂时去除bgm。 2.添加logo

---
+++ Assets/Scripts/Test1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    public Sprite sprite;
+
+    private void Start()
+    {
+        //Texture2D tex = sprite.Sprite_2_Texture2D();
+        //tex.SaveTexture2D(Application.dataPath + "/test1.png");
+    }
+
+}
