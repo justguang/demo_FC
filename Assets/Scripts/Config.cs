3e8e9e8c346b32c730b198a780759ea24ec1f551
justguang <2320366520@qq.com>
2022-08-23 16:50:30 +0800

    updatekey

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,11 +1,7 @@
-﻿public static class Config
-{
-    //###  key
-    public const string Access_Key_Secret = "80TwyzdLH2uOzFRpl299qf41z12gFV";
-    public const string Access_Key_Id = "bB4aqLzQf8oSPuuasDRoku1s";
-    public const string App_Id = "1661543510573";
-    //###  test 直播间ID 25701913
+﻿using System.Text;
 
+public static class Config
+{
     //offset
     public const float OffsetWaitPos = 65f;
     //每个阵营最多人数限制
@@ -18,5 +14,7 @@
     public const string Join_Green = "加入绿";
     public const string Join_Red = "加入红";
     #endregion
+
+
 }
 
